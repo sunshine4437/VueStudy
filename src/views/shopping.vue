a75b8c81df78643d26d3bf51235608f32221b2a0
cwh89 <usajoi@naver.com>
2021-12-31 17:55:24 +0900

    12/31

--- src/views/shopping.vue
+++ src/views/shopping.vue
@@ -5,38 +5,66 @@
         <div class="aside_info">
             <h2>가격비교</h2>
             <ul class="buy">
-              <li> <input type="checkbox" name="" id=""> <a href="" class="blank">숙박대전</a></li>
-                <li><input type="checkbox" name="" id="">배송선물</li>
-                <li>희망쇼핑</li>
-                <li>할인금액</li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">숙박대전</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">배송선물</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">희망쇼핑</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">할인금액</a></li>
             </ul>
             <h2>사이즈</h2>
             <ul class="size">
-                <li>130~140이상</li>
-                <li>145~155이상</li>
-                <li>160~170이상</li>
-                <li>175이상</li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">130~140이상</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">145~155이상</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">160~170이상</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">175이상</a></li>
             </ul>
             <h2>색상</h2>
             <ul class="color">
-                <li>흰색</li>
-                <li>검은색</li>
-                <li>빨간색</li>
-                <li>파란색</li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">블랙 계열</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">화이트 계열</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">레드 계열</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">옐로우 계열</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">그린 계열</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">블루 계열</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">브라운 계열</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">실버 계열</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">골드 계열</a></li>
             </ul>
             <h2>판매처</h2>
             <ul class="aside_vendor">
-                <li>백화점</li>
-                <li>인터넷쇼핑몰</li>
-                <li>홈쇼핑</li>
-                <li>아울렛</li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">공식인증셀러</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">대형몰</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">홈쇼핑</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">전문몰</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">해외몰</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">아울렛</a></li>
             </ul>
             <h2>배송혜택</h2>
             <ul class="aside_delivery">
-                <li>무료배송</li>
-                <li>오늘발송</li>
-                <li>반품 배송비지원</li>
-                <li>무료선물포장</li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">무료배송</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">오늘발송</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">반품 배송비지원</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">무료선물포장</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">장보기</a></li>
+            </ul>
+            <h2>브랜드</h2>
+            <ul class="aside_brand">
+                <li><input type="checkbox" class="blank"><a href="" class="blank">아디다스</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">나이키</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">리복</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">뉴발란스</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">휠라</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">퓨마</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">프로스펙스</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">르꼬끄</a></li>
+            </ul>
+             <h2>백화점</h2>
+            <ul class="aside_brand">
+                <li><input type="checkbox" class="blank"><a href="./basket.vue" class="blank">AK플라자</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">아이파크몰</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">현대백화점</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">롯데백화점</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">신세계백화점</a></li>
+                <li><input type="checkbox" class="blank"><a href="" class="blank">갤러리아백화점</a></li>
             </ul>
         </div>
     </div>
@@ -47,22 +75,22 @@
             <h2 class='span1'> 인기상품</h2>
             <div class="products">
                 <router-link v-bind:to="'/productDetail'">
-                    <img src="@/components/mainPage/productTableImage/1.jpg">
+                    <img src="@/assets/listImage/f1.jpg">
                     <p>[뉴발란스] 운동화 MW880GR4 NBPQAS102G</p>
                     <p class="price">125,000원</p>
                 </router-link>
                 <a href="#">
-                    <img src="@/components/mainPage/productTableImage/1.jpg">
+                    <img src="@/assets/listImage/f2.jpg">
                     <p>[뉴발란스] 327 스니커즈 MS327FE</p>
                     <p class="price">150,900원</p>
                 </a>
                 <a href="#">
-                    <img src="@/components/mainPage/productTableImage/1.jpg">
+                    <img src="@/assets/listImage/f3.jpg">
                     <p>아디다스 울트라부스트 21_FY0379</p>
                     <p class="price">114,500원</p>
                 </a>
                 <a href="#">
-                    <img src="@/components/mainPage/productTableImage/1.jpg">
+                    <img src="@/assets/listImage/f4.jpg">
                     <p>나이키 남성 에어맥스 90 EOI DA5562-001</p>
                     <p class="price">129,000원</p>
                 </a>
@@ -74,7 +102,7 @@
                 <ul>
                     <li class="item">
                         <a href="#">
-                            <img src="@/components/mainPage/productTableImage/1.jpg">
+                            <img src="@/assets/listImage/s1.jpg">
                         </a>
                         <div class="desc">
                             <p class="name">나이키 에어맥스97 트리플블랙</p>
@@ -88,7 +116,7 @@
                     </li>
                     <li class="item">
                         <a href="#">
-                            <img src="@/components/mainPage/productTableImage/1.jpg">
+                            <img src="@/assets/listImage/s2.jpg">
                         </a>
                         <div class="desc">
                             <p class="name">아키클래식 어반트래터 트래캥슈즈</p>
@@ -102,7 +130,7 @@
                     </li>
                     <li class="item">
                         <a href="#">
-                            <img src="@/components/mainPage/productTableImage/1.jpg">
+                            <img src="@/assets/listImage/s3.jpg">
                         </a>
                         <div class="desc">
                             <p class="name">뉴발란스 327 운동화 MS327FE</p>
@@ -116,7 +144,7 @@
                     </li>
                      <li class="item">
                         <a href="#">
-                            <img src="@/components/mainPage/productTableImage/1.jpg">
+                            <img src="@/assets/listImage/s4.jpg">
                         </a>
                         <div class="desc">
                             <p class="name">아디다스 갤럭시 5 FY6718</p>
@@ -130,7 +158,7 @@
                     </li>
                      <li class="item">
                         <a href="#">
-                            <img src="@/components/mainPage/productTableImage/1.jpg">
+                            <img src="@/assets/listImage/s5.jpg">
                         </a>
                         <div class="desc">
                             <p class="name">리복클래식 레더 레거시 GZ0741</p>
@@ -144,7 +172,7 @@
                     </li>
                     <li class="item">
                         <a href="#">
-                            <img src="@/components/mainPage/productTableImage/1.jpg">
+                            <img src="@/assets/listImage/s6.jpg">
                         </a>
                         <div class="desc">
                             <p class="name">휠라 공용 레인져 1RM01141-100</p>
@@ -152,11 +180,6 @@
                             <p class="rate"> 할인: 40,200원</p>
                             <p class="delivery"> 배송비: 2,500원</p>
                         </div>
-                    </li>
-                     <li class="item">
-                        <a href="#">
-                            <img src="@/components/mainPage/productTableImage/1.jpg">
-                        </a>
                         <div class="desc">
                             <span class="vendor"> 판매처: 홈앤쇼핑몰</span>
                         </div>
@@ -193,7 +216,7 @@ export default {
     margin: 0;
     padding: 0;
     display: inline;
-    border: 1px solid rgb(189, 185, 185);
+    border: 1px solid rgb(197, 195, 195);
     border-radius: 4px;
 }
 .products_fa{
@@ -237,12 +260,16 @@ export default {
 .products img {
     width: 200px;
     height: 200px;
-    
+    border-radius: 4px;
 }
 
 .products p {
     font-family: 맑은고딕, Malgun Gothic, dotum, gulim, sans-serif;
     font-size: 16px;
+}
+.products p:nth-child(3) {
+    font-weight: bold;
+    font-size: 22px;
 }
 
 .products1 ul {
@@ -258,10 +285,12 @@ export default {
     align-items: center;
     background-color: #fafafa;
     border-radius: 4px;
-    margin-left:1px solid rgb(189, 185, 185) ;
+    margin-left:1px solid rgb(197, 195, 195) ;
 }
 .products1 .item a>img {
     width: 200px;
+    margin-top: 8px;
+    border-radius: 4px;
 }
 
  .desc {
@@ -276,13 +305,17 @@ export default {
     margin: 0 10px;
     width: 450px;
 }
+.products1 .item .desc p:nth-child(2){
+    text-decoration: line-through;
+}
 .products1 .item .desc p:nth-child(3){
     font-weight: bold;
+    font-size: 22px;
 }
 
 .desc .vendor {
     display: inline-block;
-    border-left: 1px solid rgb(189, 185, 185);
+    border-left: 1px solid rgb(197, 195, 195);
     width: 200px;
     margin: 0;
     padding-top: 60px;
@@ -297,13 +330,8 @@ export default {
 }
 
 .aside_info {
-    border: 1px solid rgb(189, 185, 185);
-    width: 150px;
-    background-color: #fafafa;
-    border-bottom: 1px solid rgb(189, 185, 185);
-    border-radius: 4px;
-
     border: 1px solid rgb(197, 195, 195);
+    width: 250px;
     background-color: #fafafa;
     border-radius: 4px;
 }
@@ -312,12 +340,16 @@ export default {
     margin: 0;
     display: flex;
     padding-left: 10px;
-    border-bottom: 1px solid rgb(189, 185, 185);
+    border-bottom: 1px solid rgb(197, 195, 195);
     background-color: white;
     font-family: 맑은고딕, Malgun Gothic, dotum, gulim, sans-serif;
     font-size: 22px;
 }
-
+.aside_info li .blank {
+    text-decoration: none;
+    font-family: 맑은고딕, Malgun Gothic, dotum, gulim, sans-serif;
+    font-size: 18px;
+}
 .aside_info ul {
     list-style: none;
     margin: 10px;
