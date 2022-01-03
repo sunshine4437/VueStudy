ceb9bb878f2ca1879198a478a8428d9015c72957
Portal116 <tim950116@gmail.com>
2022-01-03 18:51:51 +0900

    2022-01-03 18:51

--- src/views/payment.vue
+++ src/views/payment.vue
@@ -93,7 +93,7 @@
                         placeholder="-없이 숫자만"
                         id="mobile"
                         required
-                        @change="phoneCheck()"
+                        @keyup="phoneCheck()"
                     />
                 </span>
                 <span>
@@ -105,12 +105,14 @@
             <p>
                 <span>우편번호</span>
                 <span>
-                    <input type="text" id="zipCode" class="shortInput" />
+                    <input
+                        class="shortInput"
+                        type="text"
+                        v-model="postcode"
+                        placeholder="우편번호"
+                    />
                     <span>
-                        <window-popup v-model="open">
-                            <deliverySelect></deliverySelect>
-                        </window-popup>
-                        <button class="addrBtn" @click="open = true">
+                        <button class="addrBtn" @click="execDaumPostcode()">
                             검색
                         </button>
                     </span>
@@ -119,13 +121,24 @@
             <p>
                 <span>기본주소</span>
                 <span colspan="2">
-                    <input type="text" id="baseAddr" class="longInput" />
+                    <input
+                        type="text"
+                        class="longInput"
+                        id="address"
+                        v-model="address"
+                        placeholder="주소"
+                    />
                 </span>
             </p>
             <p>
                 <span>상세주소</span>
                 <span colspan="2">
-                    <input type="text" name="detailAddr" class="longInput" />
+                    <input
+                        type="text"
+                        class="longInput"
+                        id="detailAddress"
+                        placeholder="상세주소"
+                    />
                 </span>
             </p>
         </div>
@@ -235,13 +248,7 @@
 </template>
 
 <script>
-import WindowPopup from "@/components/payment/WindowPopup.vue";
-import deliverySelect from "@/components/payment/deliverySelect.vue";
 export default {
-    components: {
-        WindowPopup,
-        deliverySelect,
-    },
     data() {
         return {
             point: 0,
@@ -254,6 +261,9 @@ export default {
             phoneValidate: true,
             usable: 3548,
             open: false,
+            postcode: "",
+            address: "",
+            extraAddress: "",
             orderProducts: [
                 {
                     number: "1",
@@ -279,8 +289,8 @@ export default {
     methods: {
         applyPoint() {
             let pt = document.getElementById("pointInput").value;
-            if (pt === '') {
-                alert('숫자를 입력해주세요')
+            if (pt === "") {
+                alert("숫자를 입력해주세요");
             } else if (pt > parseInt(this.usable) + parseInt(this.point)) {
                 alert("포인트가 부족합니다.");
             } else {
@@ -306,6 +316,52 @@ export default {
             } else {
                 this.phoneValidate = false;
             }
+        },
+        execDaumPostcode() {
+            new window.daum.Postcode({
+                oncomplete: (data) => {
+                    if (this.extraAddress !== "") {
+                        this.extraAddress = "";
+                    }
+                    if (data.userSelectedType === "R") {
+                        // 사용자가 도로명 주소를 선택했을 경우
+                        this.address = data.roadAddress;
+                    } else {
+                        // 사용자가 지번 주소를 선택했을 경우(J)
+                        this.address = data.jibunAddress;
+                    }
+
+                    // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+                    if (data.userSelectedType === "R") {
+                        // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                        // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                        if (
+                            data.bname !== "" &&
+                            /[동|로|가]$/g.test(data.bname)
+                        ) {
+                            this.extraAddress += data.bname;
+                        }
+                        // 건물명이 있고, 공동주택일 경우 추가한다.
+                        if (
+                            data.buildingName !== "" &&
+                            data.apartment === "Y"
+                        ) {
+                            this.extraAddress +=
+                                this.extraAddress !== ""
+                                    ? `, ${data.buildingName}`
+                                    : data.buildingName;
+                        }
+                        // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                        if (this.extraAddress !== "") {
+                            this.extraAddress = `(${this.extraAddress})`;
+                        }
+                    } else {
+                        this.extraAddress = "";
+                    }
+                    // 우편번호를 입력한다.
+                    this.postcode = data.zonecode;
+                },
+            }).open();
         },
     },
 };
